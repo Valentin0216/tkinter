e58961ca1330750960cd4cb13c37615f5538434e
Valentin0216 <simonvalentinmark@turr.hu>
2022-03-21 12:55:45 +0100

    padding hozzáadása a kódhoz

--- feladat.py
+++ feladat.py
@@ -1,21 +1,22 @@
 from tkinter import *
 
 window = Tk()
+window.title("Feladat")
 
 elso_mezo = Label(window, text="Első mező:")
-elso_mezo.grid(row=0, column=0)
+elso_mezo.grid(row=0, column=0, pady=5)
 
 elso_input = Entry(window)
 elso_input.grid(row=0, column=1)
 
 masodik_mezo = Label(window, text="Második mező:")
-masodik_mezo.grid(row=1, column=0)
+masodik_mezo.grid(row=1, column=0, pady=5)
 
 masodik_input = Entry(window)
 masodik_input.grid(row=1, column=1)
 
 harmadik_mezo = Label(window, text="Harmadik mező:")
-harmadik_mezo.grid(row=2, column=0)
+harmadik_mezo.grid(row=2, column=0, pady=5)
 
 harmadik_input = Entry(window)
 harmadik_input.grid(row=2, column=1)
